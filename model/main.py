b4f04713aac9ec12179511e0ca960111ad080703
benab04 <benabrahambiju@gmail.com>
2023-12-19 11:51:38 +0530

    update

--- model/main.py
+++ model/main.py
@@ -4,13 +4,16 @@ import os
 import cv2
 import matplotlib.pyplot as plt
 import pickle
+from sklearn.model_selection import train_test_split
 
 mnist=tf.keras.datasets.mnist
 
-(x_train,y_train),(x_test,y_test)=mnist.load_data()
+(x_temp,y_temp),(x_test,y_test)=mnist.load_data()
 
-x_train=tf.keras.utils.normalize(x_train,axis=1)
+x_temp=tf.keras.utils.normalize(x_temp,axis=1)
 x_test=tf.keras.utils.normalize(x_test,axis=1)
+
+x_train, x_cv, y_train, y_cv=train_test_split(x_temp, y_temp, test_size=0.2, random_state=100)
 
 model= tf.keras.models.Sequential()
 
@@ -28,10 +31,13 @@ model.summary()
 
 model.fit(x_train, y_train, epochs=8)
 
-loss, accuracy=model.evaluate(x_test, y_test)
+loss_cv, accuracy_cv=model.evaluate(x_cv,y_cv)
+loss_test, accuracy_test=model.evaluate(x_test, y_test)
 
-print(f"Loss: {loss}")
-print(f"Accuracy: {accuracy}")
+
+print(f"Cross Validation Loss: {loss_cv}, Test Loss: {loss_test}")
+print(f"Cross Validation Accuracy: {accuracy_cv}, Test Accuracy: {accuracy_test}")
+
 
 with open('model.pkl', 'wb') as f:
     pickle.dump(model,f)
